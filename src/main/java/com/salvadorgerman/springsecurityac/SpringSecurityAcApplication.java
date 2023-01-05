4545a75a760f01181d0e0949c6f6e4ba8e4e78e5
Saen German <salvadorenriquegb@gmail.com>
2023-01-04 19:10:16 -0600

    Initial commit

---
+++ src/main/java/com/salvadorgerman/springsecurityac/SpringSecurityAcApplication.java
@@ -0,0 +1,13 @@
+package com.salvadorgerman.springsecurityac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityAcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/salvadorgerman/springsecurityac/SpringSecurityAcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salvadorgerman.springsecurityac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
